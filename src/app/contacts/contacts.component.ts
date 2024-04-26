3d887489ada834663ffd260f86fe9bdfb141d3d7
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2024-04-26 20:04:30 +0200

    add contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -9,5 +9,7 @@ export class ContactsComponent {
   contacts = [
     {'name': "Alexej Hein", 'email': "test@aol.de", 'phone': "123456789"},
     {'name': "Max Mustermann", 'email': "max@ma.de", 'phone': "987654321"},
+    {'name': "Peter Pan", 'email': "Peter@fr.de", 'phone': "456789123"},
+    {'name': "Hans Wurst", 'email': "HUe@ai.de", 'phone': "789123456"},
   ]
 }
