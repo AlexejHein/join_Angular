e22d971834c2b4305e2bda9cd176ef8b1d064d66
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2024-04-26 19:59:50 +0200

    add contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent {
-
+  contacts = [
+    {'name': "Alexej Hein", 'email': "test@aol.de", 'phone': "123456789"},
+    {'name': "Max Mustermann", 'email': "max@ma.de", 'phone': "987654321"},
+  ]
 }
