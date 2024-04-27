cd522c1f7631762ba3a2cda8697a6680ac82cdb8
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2024-04-27 04:28:52 +0200

    add contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
+  animations: [
+    trigger('slideOutRight', [
+      state('in', style({transform: 'translateX(0)'})),
+      transition(':leave', [
+        animate(500, style({transform: 'translateX(100%)'}))
+      ])
+    ])
+  ]
 })
 export class ContactsComponent {
   contacts = [
@@ -12,4 +21,8 @@ export class ContactsComponent {
     {'name': "Peter Pan", 'email': "Peter@fr.de", 'phone': "456789123"},
     {'name': "Hans Wurst", 'email': "HUe@ai.de", 'phone': "789123456"},
   ]
+
+  removeContact(index: number) {
+    this.contacts.splice(index, 1);
+  }
 }
