82e07171f15103e0d18a88ce38e47991d618ccbd
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2024-04-28 04:32:07 +0200

    add contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -6,11 +6,17 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css'],
   animations: [
-    trigger('slideOutRight', [
-      state('in', style({transform: 'translateX(0)'})),
-      transition(':leave', [
-        animate(500, style({transform: 'translateX(100%)'}))
-      ])
+    trigger('slideInOut', [
+      state('out', style({
+        transform: 'translateX(100%)',
+        display: 'none'
+      })),
+      state('in', style({
+        transform: 'translateX(0)',
+        display: 'block'
+      })),
+      transition('out => in', animate('500ms ease-in')),
+      transition('in => out', animate('500ms ease-out'))
     ])
   ]
 })
@@ -20,10 +26,14 @@ export class ContactsComponent {
     {'name': "Max Mustermann", 'email': "max@ma.de", 'phone': "987654321", 'isClicked': false},
     {'name': "Peter Pan", 'email': "Peter@fr.de", 'phone': "456789123", 'isClicked': false},
     {'name': "Hans Wurst", 'email': "HUe@ai.de", 'phone': "789123456", 'isClicked': false},
-  ]
+  ];
 
   clickContact(index: number) {
-    this.contacts[index].isClicked = true;
+    // Setze alle isClicked auf false und dann den ausgewählten auf true
+    this.contacts.forEach((c, i) => {
+      if (i !== index) c.isClicked = false;
+    });
+    this.contacts[index].isClicked = !this.contacts[index].isClicked;
   }
 
   removeContact(index: number) {
