df4809f8ae2420194e2efd493ccbda15ebd26fb7
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2024-04-28 04:21:12 +0200

    add contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -16,11 +16,15 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
 })
 export class ContactsComponent {
   contacts = [
-    {'name': "Alexej Hein", 'email': "test@aol.de", 'phone': "123456789"},
-    {'name': "Max Mustermann", 'email': "max@ma.de", 'phone': "987654321"},
-    {'name': "Peter Pan", 'email': "Peter@fr.de", 'phone': "456789123"},
-    {'name': "Hans Wurst", 'email': "HUe@ai.de", 'phone': "789123456"},
+    {'name': "Alexej Hein", 'email': "test@aol.de", 'phone': "123456789", 'isClicked': false},
+    {'name': "Max Mustermann", 'email': "max@ma.de", 'phone': "987654321", 'isClicked': false},
+    {'name': "Peter Pan", 'email': "Peter@fr.de", 'phone': "456789123", 'isClicked': false},
+    {'name': "Hans Wurst", 'email': "HUe@ai.de", 'phone': "789123456", 'isClicked': false},
   ]
+
+  clickContact(index: number) {
+    this.contacts[index].isClicked = true;
+  }
 
   removeContact(index: number) {
     this.contacts.splice(index, 1);
