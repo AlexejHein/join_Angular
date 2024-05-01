c2035633f7dd522a76355154523144f7a2d6d4b7
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2024-05-01 14:42:47 +0200

    add contacts dialog

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class AddTaskComponent {
   value: any;
 
+  contacts = [
+    {'id': 1, 'name': "Alexej Hein", 'email': "test@aol.de", 'phone': "123456789", 'isClicked': false},
+    {'id': 2, 'name': "Max Mustermann", 'email': "max@ma.de", 'phone': "987654321", 'isClicked': false},
+    {'id': 3, 'name': "Peter Pan", 'email': "Peter@fr.de", 'phone': "456789123", 'isClicked': false},
+    {'id': 4, 'name': "Hans Wurst", 'email': "HUe@ai.de", 'phone': "789123456", 'isClicked': false},
+  ];
+
 }
