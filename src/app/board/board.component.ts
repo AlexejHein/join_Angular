b657ed4225bbbf69c30507a6622de47cbbdba279
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2024-05-07 07:00:02 +0200

    add task dialog

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -11,17 +11,17 @@ import { MatDialog } from '@angular/material/dialog';
 export class BoardComponent {
   tasks = {
     todo: [
-      { header: 'Hello', title: 'Task 1', content: 'Lorem ipsum dolor sit amet...', date: '1/4', person: 'AH', data: '29/07/2024', preority: 'low'},
-      { header: 'Hi', title: 'Task 2', content: 'Lorem ipsum dolor sit amet...', date: '2/4', person: 'BH', data: '29/07/2024', preority: 'low' }
+      { header: 'Hello', title: 'Task 1', content: 'Lorem ipsum dolor sit amet...', date: '1/4', person: 'AH', data: '29/07/2024', priority: 'low'},
+      { header: 'Hi', title: 'Task 2', content: 'Lorem ipsum dolor sit amet...', date: '2/4', person: 'BH', data: '29/07/2024', priority: 'low' }
     ],
     inProgress: [
-      { header: 'Test', title: 'Task 3', content: 'Lorem ipsum dolor sit amet...', date: '3/4', person: 'CH', data: '29/07/2024', preority: 'middle' }
+      { header: 'Test', title: 'Task 3', content: 'Lorem ipsum dolor sit amet...', date: '3/4', person: 'CH', data: '29/07/2024', priority: 'middle' }
     ],
     awaitingFeedback: [
-      { header: 'Review', title: 'Task 4', content: 'Lorem ipsum dolor sit amet...', date: '4/4', person: 'DH', data: '13/04/2024', preority: 'urgent' }
+      { header: 'Review', title: 'Task 4', content: 'Lorem ipsum dolor sit amet...', date: '4/4', person: 'DH', data: '13/04/2024', priority: 'urgent' }
     ],
     done: [
-      { header: 'Completed', title: 'Task 5', content: 'Lorem ipsum dolor sit amet...', date: '5/4', person: 'EH', data: '29/07/2024', preority: 'low' }
+      { header: 'Completed', title: 'Task 5', content: 'Lorem ipsum dolor sit amet...', date: '5/4', person: 'EH', data: '29/07/2024', priority: 'low' }
     ]
   };
 
