f19fc96b4a1de0e48224d91eca32bd50520b602c
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2024-06-12 17:16:02 +0200

    add backend

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -16,7 +16,8 @@ export class SummaryComponent {
     inProgress: 0,
     awaitingFeedback: 0,
     done: 0,
-    total: 0
+    total: 0,
+    urgent: 0
   };
 
   constructor(private taskService: TaskService) {
@@ -33,7 +34,8 @@ export class SummaryComponent {
       this.taskCounts.awaitingFeedback = this.tasks.filter(task => task.status === 'awaitingFeedback').length;
       this.taskCounts.done = this.tasks.filter(task => task.status === 'done').length;
       this.taskCounts.total = this.tasks.length;
-      this.urgentTask = data.find(task => task.status === 'urgent') || data.sort((a, b) => new Date(a.due_date).getTime() - new Date(b.due_date).getTime())[0];});
+      this.taskCounts.urgent = this.tasks.filter(task => task.priority === 'urgent').length;
+      this.urgentTask = data.find(task => task.priority === 'urgent') || data.sort((a, b) => new Date(a.due_date).getTime() - new Date(b.due_date).getTime())[0];});
   }
 
 }
